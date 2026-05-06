c970d868919a5141f302387b3b31f450eedb8d99
RShems6000 <rajput.shamshed@gmail.com>
2026-05-06 11:40:02 +0000

    Mise à jour de 3-gen_key.sh uniquement

--- cybersecurity_basics/0x00_introduction_cybersecurity/3-gen_key.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/3-gen_key.sh
@@ -1,18 +1,2 @@
-#!/bin/bash
-
-# Affiche un message sympa
-echo "🔐 Génération d'une clé SSH RSA 4096"
-
-# Nom de la clé (tu peux changer)
-NOM_CLE="ma_cle_rsa"
-
-# La commande magique
+#!/bin/bashgit 
 ssh-keygen -t rsa -b 4096 -f "$NOM_CLE" -N ""
-
-# Message de fin
-echo "✅ Clé générée avec succès !"
-echo "📁 Clé privée : $NOM_CLE"
-echo "📁 Clé publique : $NOM_CLE.pub"
-
-# Affiche les fichiers créés
-ls -l "$NOM_CLE"*

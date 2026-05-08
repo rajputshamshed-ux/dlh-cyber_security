74d6bd55a4d2f83640425a5b95337fe246d2ad44
RShems6000 <rajput.shamshed@gmail.com>
2026-05-08 10:55:18 +0000

    Fix 2-incoming_connections.sh: reduce to exactly 2 lines with && operator

--- linux_security/0x00_linux_security_basics/2-incoming_connections.sh
+++ linux_security/0x00_linux_security_basics/2-incoming_connections.sh
@@ -1,10 +1,2 @@
-#!/bin/bash                    # 1. Dis à l'ordi : "Parle en bash"
-# Autoriser uniquement...     # 2. Petit rappel pour toi (ignoré)
-sudo iptables -A INPUT \       # 3. Ajoute une règle pour connexions entrantes
-       -p tcp \                #    - protocole TCP
-       --dport 80 \            #    - porte destination 80
-       -j ACCEPT               #    - laisse passer
-sudo ip6tables -A INPUT \      # 4. La même chose pour les nouvelles adresses (IPv6)
-       -p tcp \
-       --dport 80 \
-       -j ACCEPT
+#!/bin/bash
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT && sudo ip6tables -A INPUT -p tcp --dport 80 -j ACCEPT

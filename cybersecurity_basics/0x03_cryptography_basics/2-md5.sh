c0022557693dafff30fc606dee8a464ae5fc4b69
RShems6000 <rajput.shamshed@gmail.com>
2026-05-18 07:41:05 +0000

    cybersecurity_basics/0x03_cryptography_basics/2-md5.sh

--- cybersecurity_basics/0x03_cryptography_basics/2-md5.sh
+++ cybersecurity_basics/0x03_cryptography_basics/2-md5.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-# On prend le mot de passe que tu as donné
 password="$1"
-
-# On utilise la formule magique MD5
 hash=$(echo -n "$password" | md5sum)
-
-# On nettoie un peu (enlève le " -" à la fin)
 hash=${hash%% *}
-
-# On écrit le résultat dans le fichier
-echo "$hash" > 2_hash.txt
+echo -e "$hash\n" > 2_hash.txt

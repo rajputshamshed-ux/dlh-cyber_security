9ec94497feb7995b1540d4c0ce9504c084e13aa0
RShems6000 <rajput.shamshed@gmail.com>
2026-05-18 08:35:15 +0000

    /workspaces/dlh-cyber_security/cybersecurity_basics/0x03_cryptography_basics

--- cybersecurity_basics/0x03_cryptography_basics/2-md5.sh
+++ cybersecurity_basics/0x03_cryptography_basics/2-md5.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-password="$1"
-echo -n "$password" | md5sum | cut -d' ' -f1 > 2_hash.txt
-echo "" >> 2_hash.txt
+echo -n "$1" | md5sum | cut -d' ' -f1 > 2_hash.txt; echo "" >> 2_hash.txt

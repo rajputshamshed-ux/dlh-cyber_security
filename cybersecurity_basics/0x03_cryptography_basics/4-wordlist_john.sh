08e345c42897d37a39c730ecbda252853d40d469
RShems6000 <rajput.shamshed@gmail.com>
2026-05-19 08:00:49 +0000

    Add John the Ripper wordlist mode cracker

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-cd /tmp/john/run
-./john --format=raw-md5 --wordlist=/usr/share/wordlists/rockyou.txt /workspaces/dlh-cyber_security/cybersecurity_basics/0x03_cryptography_basics/hash.txt
-./john --show /workspaces/dlh-cyber_security/cybersecurity_basics/0x03_cryptography_basics/hash.txt | grep -v "password hash" | cut -d ':' -f2 | cut -d ' ' -f1 > /workspaces/dlh-cyber_security/cybersecurity_basics/0x03_cryptography_basics/4-password.txt
-cat /workspaces/dlh-cyber_security/cybersecurity_basics/0x03_cryptography_basics/4-password.txt
+J="/workspaces/dlh-cyber_security/cybersecurity_basics/0x03_cryptography_basics"; cd /tmp/john/run && ./john --format=raw-md5 --wordlist=/usr/share/wordlists/rockyou.txt $J/hash.txt && ./john --show $J/hash.txt | grep -v "password hash" | cut -d ':' -f2 | cut -d ' ' -f1 > $J/4-password.txt

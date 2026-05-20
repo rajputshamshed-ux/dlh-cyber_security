0936bb36e6c95161610a3a4ce7d891ad52018811
rajputshamshed-ux <rajput.shamshed@gmail.com>
2026-05-20 18:57:57 +0200

    update v2 exercices 6

--- cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/6-crack_john.sh
@@ -1,8 +1,2 @@
-# Étape 1 : choisir un mot de passe
-password="chocolat"
-
-# Étape 2 : générer son hash SHA-256
-hash=$(echo -n "$password" | sha256sum | cut -d' ' -f1)
-
-# Étape 3 : écrire le hash dans crack.txt
-echo "$hash" > crack.txt
+#!/bin/bash
+john --show "$1" | grep -v ":" > 6-password.txt

a71328404ff2736cea807808cc3848fb631e9b14
rajputshamshed-ux <rajput.shamshed@gmail.com>
2026-05-26 22:54:08 +0200

    1firewall.sh V2

--- linux_security/network_protocols/1-firewall.sh
+++ linux_security/network_protocols/1-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo iptables -A  INPUT -p tcp --dport 22 - j ACCEPT
-sudo iptables -A OUTPUT -p tcp --dport 22 - j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT

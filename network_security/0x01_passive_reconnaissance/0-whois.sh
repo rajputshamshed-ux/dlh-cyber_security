f3905417e71019088cef0a34fa5a1a6c85a4ac46
rajputshamshed-ux <rajput.shamshed@gmail.com>
2026-05-27 00:02:23 +0200

    V1

---
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+whois "$1" | awk -F: '/^(Registrant|Admin|Tech) / {field = $1; value = ""; for (i = 2; i <= NF; i++) {if (i > 2) value = value ":"; value = value $i} gsub(/^ /, "", value); if (field ~ /Street/) value = value " "; if (field ~ /Ext/) { field = field ":"; value = "" } printf "%s,%s\n", field, value}

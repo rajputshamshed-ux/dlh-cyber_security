f71dc213efb0eed8be18518c50b719ee5b03971d
rajputshamshed-ux <rajput.shamshed@gmail.com>
2026-06-02 21:12:20 +0200

    Python exo 2 v1

---
+++ scripting_cyber/0x01-python_scripting/2-download_page.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import requests
+from bs4 import BeautifulSoup
+
+def download_page(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        return soup.prettify()
+    except requests.exceptions.RequestException as e:
+        return str(e)

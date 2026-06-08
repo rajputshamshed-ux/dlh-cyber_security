c15b78625af8dc97c8928426af06af772931b7a2
rajputshamshed-ux <rajput.shamshed@gmail.com>
2026-06-08 09:56:17 +0200

    exo 0 XOR base 64 v1

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,19 +1,4 @@
-#!/bin/bash
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 {xor}HASH"
-    exit 1
-fi
-
-hash="${1#\{xor\}}"
-decoded=$(echo "$hash" | base64 -d 2>/dev/null)
-
-result=""
-for (( i=0; i<${#decoded}; i++ )); do
-    char="${decoded:$i:1}"
-    ascii=$(printf "%d" "'$char")
-    xor=$((ascii ^ 0x5F))
-    result+=$(printf "\\$(printf '%03o' $xor)")
-done
-
-echo "$result"
+echo "$1" \
+| sed 's/^{xor}//' \
+| base64 -d \
+| perl -pe '$_ ^= "\x5A" x length($_)'
